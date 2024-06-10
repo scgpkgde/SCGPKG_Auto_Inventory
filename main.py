c68b7bc96954dbf4339868433cd1b9b034bff03c
benjamai <benjamai@scg.com>
2024-06-10 10:20:06 +0700

    update

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ PAGES = {
     "Duplex system": duplex_system_app,
 }
 st.sidebar.write(
-        f'<h2 style="color: #162e74;font-weight: 600;">PP Inventory',
-        f'</h2>',
+        f'<div style="margin-top: -35px;font: initial;font-size: 40px;color: #162e74;">PP Inventory',
+        f'</div>',
         unsafe_allow_html=True
     )
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 st.sidebar.markdown('<hr style="margin-top: 5px; margin-bottom:15px;">', unsafe_allow_html=True)
 st.sidebar.write(
-        f'<h2 style="color: #162e74;font-weight: 800;">{selection}',
+        f'<h2 style="color: #162e74;font: initial;font-size: 20px; margin-top: -10px">{selection}',
         f'</h2>',
         unsafe_allow_html=True
     )
